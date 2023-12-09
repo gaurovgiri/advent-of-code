42b416768d317bdce0bad4eddabdfa4d7118d036
gaurovgiri <gaurovgiri@gmail.com>
2023-12-09 23:27:33 +0545

    add day 2 solution

---
+++ 2023/Day2/solution1.py
@@ -0,0 +1,32 @@
+
+# ids : possible games
+# red, green, blue
+
+games = []
+with open("Day2/test.txt",'r') as puzzle_inp:
+    for game in puzzle_inp.readlines():
+        games.append(game[:-1])
+
+
+sum = 0
+for game in games:
+    game = game[5:]
+    gid = game.split(":")[0]
+    subgames = game.split(":")[1].replace(" ","").split(";")
+    red = blue = green = 0
+    valid = True
+    for cubes in subgames:
+        for cube in cubes.split(","):
+            if "red" in cube:
+                curr_red = int(cube.split("red")[0])
+                red =  curr_red if curr_red > red else red
+            elif "blue" in cube:
+                curr_blue = int(cube.split("blue")[0])
+                blue = curr_blue if curr_blue > blue else blue
+            elif "green" in cube:
+                curr_green = int(cube.split("green")[0])
+                green = curr_green if curr_green > green else green
+    if red <= 12 and green <= 13 and blue <= 14:
+        sum += int(gid) if valid else 0
+
+print(sum)
---
+++ 2023/Day2/solution2.py
@@ -0,0 +1,31 @@
+
+# ids : possible games
+# red, green, blue
+
+games = []
+with open("Day2/input.txt",'r') as puzzle_inp:
+    for game in puzzle_inp.readlines():
+        games.append(game[:-1])
+
+
+sum = 0
+for game in games:
+    game = game[5:]
+    gid = game.split(":")[0]
+    subgames = game.split(":")[1].replace(" ","").split(";")
+    red = blue = green = 0
+    for cubes in subgames:
+        for cube in cubes.split(","):
+            if "red" in cube:
+                curr_red = int(cube.split("red")[0])
+                red =  curr_red if curr_red > red else red
+            elif "blue" in cube:
+                curr_blue = int(cube.split("blue")[0])
+                blue = curr_blue if curr_blue > blue else blue
+            elif "green" in cube:
+                curr_green = int(cube.split("green")[0])
+                green = curr_green if curr_green > green else green
+    
+    sum += (red * blue * green)
+
+print(sum)
