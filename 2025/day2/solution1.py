98ff780e97a25a5812ecc24eb826339390a6efb6
Gaurav Giri <gaurovgiri@gmail.com>
2025-12-03 12:10:13 +0545

    2025: day 2

---
+++ 2025/day2/solution1.py
@@ -0,0 +1,28 @@
+
+def is_invalid(num):
+    s = str(num)
+    n = len(s)
+
+    for i in range(1, n//2+1):
+        if n%i == 0:
+            if s[:i] * (n//i) == s:
+                if n//i == 2:
+                    return True
+    return False
+
+def solution(input_text):
+    total = 0
+    id_ranges = input_text.split(',')
+    for id_range in id_ranges:
+        start, end = map(int, id_range.split('-'))
+        for num in range(start, end+1):
+            if is_invalid(num):
+                total += num
+    return total
+
+
+if __name__ == "__main__":
+    with open("2025/day2/input.txt", 'r') as f:
+        inputs = f.read()
+    
+    print(solution(inputs))
